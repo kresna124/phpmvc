4b18650eeefe468fc62afdbffe5ad52bf713f50c
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-05 15:36:11 +0700

    menambahkan bootstrap

---
+++ app/core/Constants.php
@@ -0,0 +1,4 @@
+<?php 
+
+
+define('BASEURL','https://localhost/phpmvc/public');
--- app/views/tamplates/header.php
+++ app/views/tamplates/header.php
@@ -2,5 +2,6 @@
 <html>
 <head>
 	<title>halaman <?= $data['judul']  ?></title>
+	<link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>/css/bootstrap.css">
 </head>
 <body>
