79a6fefe1217dcbc0a11bd03f822568f1b9a6122
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-05 14:24:40 +0700

    menambhakan controler

---
+++ app/controllers/About.php
@@ -0,0 +1,10 @@
+<?php 
+
+class About{
+	public function index($nama,$pekerjaan){
+		echo "hallo, nama saya $nama pekernaajn $pekernaajn ";
+	}
+	public function page(){
+		echo "About/page";
+	}
+}
