2ea1da07d0dd6df0c6e3939209512c69f4765e09
kresna nur ito gunawan <kresna_173040129@mail.unpas.ac.id>
2019-10-07 04:02:51 +0700

    menambahkan aksi ubah belum beres ajax tidak jalan

--- app/views/mahasiswa/index.php
+++ app/views/mahasiswa/index.php
@@ -7,7 +7,7 @@
 	</div>
 	<div class="row">
 		<div class="col-lg-6">
-			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formModal">
+			<button type="button" class="btn btn-primary tombolTambahData" data-toggle="modal" data-target="#formModal">
   Tambah data mahasiswa
 </button>
 			<br><br>
@@ -19,6 +19,7 @@
 					  		<li class="list-group-item"><?= $mhs['nama'];  ?>
 					  		
 					  		<a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id'] ?>" class="badge badge-danger float-right ml-1" onclick="return confirm('yakin?');">hapus</a>
+					  		<a href="<?= BASEURL; ?>/mahasiswa/ubah/<?= $mhs['id'] ?>" class="badge badge-success float-right ml-1 tampilModalUbah" data-toggle="modal" data-target="#formModal" data-id="<?= $mhs['id']; ?>">ubah</a>
 					  		<a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id'] ?>" class="badge badge-primary float-right ml-1">detail</a> 
 					  		</li>
 					  <?php endforeach; ?>
@@ -34,13 +35,14 @@
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="dudulModal">tambah data mahasiswa</h5>
+        <h5 class="modal-title" id="formModalLabel">tambah data mahasiswa</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
       </div>
       <div class="modal-body">
         <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+        	<input type="hidden" name="id" id="id">
         	 <div class="form-group">
 			    <label for="nama">Nama</label>
 			    <input type="text" class="form-control" id="nama" name=" nama" >
